87c27d573c0b5ad66b76789e42254064a7ccdd46
CHETAN GOUR <chetangour88@gmail.com>
2022-04-11 16:41:41 -0700

    User login validation completed

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # debug = True means whenever there is a change in the backend code the server should restart
-    app.run(debug = True)
+    app.run(debug = True) 
--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password!', category='error')
+        else:
+            flash('Email does not exists!', category='error')
 
     return render_template("login.html", user="Tim")
 
@@ -24,7 +36,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists!',category='error' )
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters!', category='error')
         elif len(first_name) < 2:
             flash('Email must be greater than 1 character!', category='error')
