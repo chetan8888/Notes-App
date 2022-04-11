499cb91e67f8c834a8cb783d604eee064f56842d
CHETAN GOUR <chetangour88@gmail.com>
2022-04-10 21:16:02 -0700

    home and auth route setup

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    # debug = True means whenever there is a change in the backend code the server should restart
     app.run(debug = True)
