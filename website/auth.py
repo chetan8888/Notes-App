4a0ebe393731ea47b79533ecd796fad6df0ca05d
CHETAN GOUR <chetangour88@gmail.com>
2022-04-11 16:56:58 -0700

    User authentication added to the view templates

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -16,17 +17,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password!', category='error')
         else:
             flash('Email does not exists!', category='error')
 
-    return render_template("login.html", user="Tim")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -51,7 +55,8 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account created!", category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user="tim")
+    return render_template("sign_up.html", user=current_user)
